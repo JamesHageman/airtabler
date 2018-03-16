24ff3cbb07eeadc075cf395ff3ed2c834739bee3
James Hageman <jamesd@hageman.ca>
2018-03-16 18:32:24 -0400

    working proxy

--- main.go
+++ main.go
@@ -6,12 +6,11 @@ import (
 	"net/http"
 	"net/url"
 	"os"
-	"strings"
 )
 
 var (
 	apiKey  = os.Getenv("AIRTABLE_API_KEY")
-	baseUrl = "https://api.airtable.com/v0/appMDlUpKSJNcvCsm"
+	baseURL = "https://api.airtable.com/v0/appMDlUpKSJNcvCsm"
 	// Hop-by-hop headers. These are removed when sent to the backend.
 	// http://www.w3.org/Protocols/rfc2616/rfc2616-sec13.html
 	hopHeaders = []string{
@@ -38,28 +37,12 @@ func die(err error) {
 	}
 }
 
-func deleteHopHeaders(header http.Header) {
-	for _, h := range hopHeaders {
-		header.Del(h)
-	}
-}
-
 func copyHeader(dst, src http.Header) {
 	for k, vv := range src {
 		for _, v := range vv {
 			dst.Add(k, v)
 		}
 	}
-}
-
-func appendHostToXForwardHeader(header http.Header, host string) {
-	// If we aren't the first proxy retain prior
-	// X-Forwarded-For information as a comma+space
-	// separated list and fold multiple headers into one.
-	if prior, ok := header["X-Forwarded-For"]; ok {
-		host = strings.Join(prior, ", ") + ", " + host
-	}
-	header.Set("X-Forwarded-For", host)
 }
 
 func main() {
@@ -70,19 +53,22 @@ func main() {
 	log.Fatal(http.ListenAndServe(addr, nil))
 }
 
-func handler(w http.ResponseWriter, req *http.Request) {
+func createProxiedRequest(req *http.Request) *http.Request {
 	q := req.URL.Query()
 	q.Add("api_key", apiKey)
-	reqURL, _ := url.Parse(baseUrl + req.URL.Path + "?" + q.Encode())
+	reqURL, _ := url.Parse(baseURL + req.URL.Path + "?" + q.Encode())
 
-	req.URL = reqURL
-	req.RequestURI = ""
+	return &http.Request{
+		Method: req.Method,
+		URL:    reqURL,
+	}
+}
 
-	deleteHopHeaders(req.Header)
-
+func handler(w http.ResponseWriter, req *http.Request) {
+	airtableReq := createProxiedRequest(req)
 	client := &http.Client{}
 
-	res, err := client.Do(req)
+	res, err := client.Do(airtableReq)
 	if err != nil {
 		http.Error(w, res.Status, res.StatusCode)
 		log.Println(err)
@@ -90,10 +76,9 @@ func handler(w http.ResponseWriter, req *http.Request) {
 	}
 	defer res.Body.Close()
 
-	deleteHopHeaders(res.Header)
 	copyHeader(w.Header(), res.Header)
 
-	log.Println(res.StatusCode, req.URL)
+	log.Println(res.StatusCode, airtableReq.URL)
 	w.WriteHeader(res.StatusCode)
 	io.Copy(w, res.Body)
 }
